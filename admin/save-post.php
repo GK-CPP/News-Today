c3238b5b9f8a7a78e2b41cd1ff06b7c638bf5186
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-10 12:19:08 +0600

    image upload

--- admin/save-post.php
+++ admin/save-post.php
@@ -1,36 +1,40 @@
 <?php
+include "utilis.php";
 include "config.php";
 
 session_start();
 
-// try {
+try {
 
-//     /*********find any error here******** */
-//     if (isset($_FILES['fileToUpload'])) {
-//         $errors = array();
-//         $file_name = $_FILES['fileToUpload']['name'];
-//         $file_size = $_FILES['fileToUpload']['size'];
-//         $file_tmp = $_FILES['fileToUpload']['tmp_name'];
-//         $file_type = $_FILES['fileToUpload']['type'];
-//         $file_ext = strtolower(end(explode('.', $_FILES['fileToUpload']['name'])));
+    /*********find any error here******** */
+    if (isset($_FILES['fileToUpload'])) {
+        $errors = array();
+        $file_name = $_FILES['fileToUpload']['name'];
+        $file_size = $_FILES['fileToUpload']['size'];
+        $file_tmp = $_FILES['fileToUpload']['tmp_name'];
+        $file_type = $_FILES['fileToUpload']['type'];
+        $file_ext = strtolower(end(explode('.', $file_name)));
+        $expensions = array("jpeg", "jpg", "png");
 
-//         $expensions = array("jpeg", "jpg", "png");
+        if (in_array($file_ext, $expensions) === false) {
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        }
 
-//         if (in_array($file_ext, $expensions) === false) {
-//             $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
-//         }
+        if ($file_size > 2097152) {
+            $errors[] = 'File size must be excately 2 MB';
+        }
 
-//         if (empty($errors) == true) {
-//             move_uploaded_file($file_tmp, "uploads/" . $file_name);
-//         } else {
-//             print_r($errors);
-//             die();
-//         }
-//     }
-// } catch (Exception $e) {
-//     echo $e->getMessage();
-//     die();
-// }
+        if (empty($errors) == true) {
+            move_uploaded_file($file_tmp, "upload/" . $file_name);
+        } else {
+            print_r($errors);
+            die();
+        }
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
 
 $title = mysqli_real_escape_string($conn, $_POST['post_title']);
 $description = mysqli_real_escape_string($conn, $_POST['postdesc']);
@@ -38,7 +42,7 @@ $category = mysqli_real_escape_string($conn, $_POST['category']);
 $date = date("d M, Y");
 $author = $_SESSION['id'];
 
-$sql = "INSERT INTO post (title, description, category, post_date, author) VALUES ('{$title}', '{$description}', '{$category}', '{$date}', '{$author}');";
+$sql = "INSERT INTO post (title, description, category, post_date, author, post_img) VALUES ('{$title}', '{$description}', '{$category}', '{$date}', '{$author}', '{$file_name}');";
 $sql .= "update catalog set post = post + 1 where category_id = {$category}";
 if ($conn->multi_query($sql) === TRUE) {
     header("Location: {$hostname}/admin/post.php");
